91b087a51393a3977c89b54e01ca355b6ba16f6f
DarrenDoan <doandarren95@gmail.com>
2024-04-15 17:13:56 +1000

    edit files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,8 @@
 import { openDB } from 'idb';
 
+const DB_NAME = 'jate';
+const DB_VER = 1;
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +16,49 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to update or insert content into the database
+export const putDb = async (content) => {
+  // Open the database with the specified name and version
+  const database = await openDB(DB_NAME, DB_VER);
+
+  // Create a readwrite transaction to make changes to the database
+  const transaction = database.transaction(DB_NAME, 'readwrite');
+
+  // Access the object store managed by this transaction
+  const store = transaction.objectStore(DB_NAME);
+
+  // Use the put method to update the content; if the record doesn't exist, it creates a new one
+  const updateResult = await store.put({ id: 1, content });
+
+  // Ensure the transaction completes successfully
+  await transaction.done;
+
+  // Return the result of the put operation
+  return updateResult;
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Function to retrieve content from the database
+export const getDb = async () => {
+  // Open the database with the specified name and version
+  const database = await openDB(DB_NAME, DB_VER);
+
+  // Create a readonly transaction to access the data
+  const transaction = database.transaction(DB_NAME, 'readonly');
+
+  // Access the object store for reading
+  const store = transaction.objectStore(DB_NAME);
+
+  // Retrieve all records from the store
+  const allRecords = await store.getAll();
+
+  // We expect to find at least one record, take the first one if available
+  const firstRecord = allRecords[0];
+
+  // Return the content of the first record, or undefined if no records exist
+  return firstRecord ? firstRecord.content : undefined;
+};
+
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html", // Required to use the existing file, or else it builds its own
+        favicon: "favicon.ico" // Making sure the favicon is included too
+      }),
+      new WebpackPwaManifest({
+        short_name: "JATE",
+        name: "Just Another Text Editor (JATE)",
+        description: "A handy text editor that can be used both online and offline",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: '#ffffff',
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: './src/images/logo.png',
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: 'assets/icons' // Referenced location in header markup
+          }
+        ],
+        fingerprints: false, // Stops adding HEX prints in file names, so icon paths work
+        filename: 'manifest.json'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-transform-runtime',
+                '@babel/plugin-proposal-object-rest-spread'
+              ]
+            }
+          }
+        }
       ],
     },
   };
